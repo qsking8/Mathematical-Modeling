824d2a7d29f22720cfe362976e415e1bd37285e6
Dze_kong <653499363@qq.com>
2024-01-27 22:44:37 +0800

    LSTM笔记以及应用

---
+++ LSTM预测/LSTM工具箱/Run_Me_Lstm.m
@@ -0,0 +1,18 @@
+% Forecast of time series based on LSTM deep learning network 
+% (supporting multiple time series)
+% 基于LSTM深度学习网络对时间序列进行预测（支持多元时间序列）
+
+% Requires:  Windows computers and  MATLAB version 2021a or later!
+% Deep Learning Toolbox is also needed.
+% Unzip the code before running it and don't forget to change current folder.
+% 要求：Windows电脑和MATLAB 2021a版本或更高版本!
+% 还需要深度学习工具箱 Deep Learning Toolbox
+% 运行代码前要解压压缩包，不要忘记更改当前文件夹
+
+
+P_LSTM_Main_Function
+
+
+% The language of the interface can be switched in the bottom 
+% right corner of the toolbox, supporting Chinese and English.
+% 工具箱右下角可以切换界面的语言，支持中文和英文。
